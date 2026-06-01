c93320a2d2212e6fe0eb86b5be30776b6e0230ea
norumai01 <johnnydinhnguyen2001@gmail.com>
2026-06-01 00:37:46 -0400

    PLEASSE FIX THISS BULLSHIT ENIVRONMENT VARIABLES VERCEL

--- api/config.js
+++ api/config.js
@@ -28,6 +28,9 @@ async function getChannelConfig(req, res) {
   const REDIS_URL = process.env.KV_REST_API_URL
   const REDIS_TOKEN = process.env.KV_REST_API_TOKEN
 
+  console.log("Redis URL: ", REDIS_URL);
+  console.log("Redis Token: ", REDIS_TOKEN);
+
   const { channelId } = req.query;
 
   if (!channelId) {
@@ -47,6 +50,9 @@ async function getChannelConfig(req, res) {
 async function saveChannelConfig(req, res) {
   const REDIS_URL = process.env.KV_REST_API_URL
   const REDIS_TOKEN = process.env.KV_REST_API_TOKEN
+
+  console.log("Redis URL: ", REDIS_URL);
+  console.log("Redis Token: ", REDIS_TOKEN);
 
   // Verify JWT so only the broadcaster can save
   const authHeader = req.headers.authorization || req.headers.Authorization;
